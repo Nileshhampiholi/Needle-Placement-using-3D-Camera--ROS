3bbad4f132d8d0bcf99a99aac85288ec44a41c19
Bannigo <64646090+Bannigo@users.noreply.github.com>
2020-06-02 11:16:02 +0530

    Add files via upload

--- panda_joint_reader/scripts/inverse.py
+++ panda_joint_reader/scripts/inverse.py
@@ -1,71 +1,44 @@
 #!/usr/bin/env python
 
-import math
-import numpy as np
-import sympy as sym
-import mpmath as mp
-from sympy.physics.mechanics import dynamicsymbols
+import sys
+import copy
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+from math import pi
+from std_msgs.msg import String
+from moveit_commander.conversions import pose_to_list
 
-def simplify_matrix(matrix):
-    mbee=       [[matrix[0,0].simplify(), matrix[0,1].simplify(), matrix[0,2].simplify(),matrix[0,3].simplify()],
-                 [matrix[1,0].simplify(), matrix[1,1].simplify(), matrix[1,2].simplify(), matrix[1,3].simplify()],
-                 [matrix[2,0].simplify(), matrix[2,1].simplify(),matrix[2,2].simplify(), matrix[2,3].simplify()],
-                 [0  ,                    0    ,                    0     ,      1      ]]
-    return mbee
+if __name__ == '__main__':
+    moveit_commander.roscpp_initialize(sys.argv)
+    rospy.init_node('inverse_kinematics', anonymous=True)
 
-theta1, theta2, theta3, theta4, theta5, theta6, theta7, theta = dynamicsymbols('theta1 theta2 theta3 theta4 theta5 theta6 theta7 theta ')
+    robot = moveit_commander.RobotCommander()
 
+    scene = moveit_commander.PlanningSceneInterface()
 
+    group_name = "panda_arm"
+    move_group = moveit_commander.MoveGroupCommander(group_name)
 
-T0_1 =  sym.Matrix([[sym.cos(theta1),                 -sym.sin(theta1),                    0,                0    ],
-         [sym.sin(theta1)*1,  sym.cos(theta1)* 1,  -0, -0*0.333],
-         [sym.sin(theta1)*0,  sym.cos(theta1)* 0,   1,  1*0.333],
-         [0  ,                  0    ,                0     ,           1    ]])
+    pose_goal = geometry_msgs.msg.Pose()
+    pose_goal.orientation.w = 0.184445195028
+    pose_goal.position.x = 0.437686078966
+    pose_goal.position.y = 0.138975159367
+    pose_goal.position.z = 0.480383719667
 
-T1_2 =   sym.Matrix([[sym.cos(theta2),                 -sym.sin(theta2),                    0,                0    ],
-         [sym.sin(theta2)*0,  sym.cos(theta2)* 0,  1, 1*0],
-         [sym.sin(theta2)*-1,  sym.cos(theta2)* -1,   0,  0*0],
-         [0  ,                  0    ,                0     ,           1    ]])
+    
+    move_group.set_pose_target(pose_goal)
 
-T2_3 =   sym.Matrix([[sym.cos(theta3),                 -sym.sin(theta3),                    0,                0    ],
-         [sym.sin(theta3)*0,  sym.cos(theta3)* 0,  -1, -1*0.316],
-         [sym.sin(theta3)*1,  sym.cos(theta3)* 1,   0,  0*0.316],
-         [0  ,                  0    ,                0     ,           1    ]])
+    plan = move_group.go(wait=True)
+    move_group.stop()
 
-T3_4 =   sym.Matrix([[sym.cos(theta4),                 -sym.sin(theta4),                    0,                0.0825    ],
-         [sym.sin(theta4)*0,  sym.cos(theta4)* 0,  -1, -1*0],
-         [sym.sin(theta4)*1,  sym.cos(theta4)* 1,   0,  0*0],
-         [0  ,                  0    ,                0     ,           1    ]])
-
-T4_5 =   sym.Matrix([[sym.cos(theta5),                 -sym.sin(theta5),                    0,                -0.0825    ],
-         [sym.sin(theta5)*0,  sym.cos(theta5)* 0,  1, 1*0.384],
-         [sym.sin(theta5)*-1,  sym.cos(theta5)* -1,   0,  0*0.384],
-         [0  ,                  0    ,                0     ,           1    ]])
-
-T5_6 =   sym.Matrix([[sym.cos(theta6),                 -sym.sin(theta6),                    0,               0    ],
-         [sym.sin(theta6)*0,  sym.cos(theta6)* 0,  -1, -1*0],
-         [sym.sin(theta6)*1,  sym.cos(theta6)* 1,   0,  0*0],
-         [0  ,                  0    ,                0     ,           1    ]])
-
-T6_7 =   sym.Matrix([[sym.cos(theta7),                 -sym.sin(theta7),                    0,               0.088    ],
-         [sym.sin(theta7)*0,  sym.cos(theta7)* 0,  -1, -1*0],
-         [sym.sin(theta7)*1,  sym.cos(theta7)* 1,   0,  0*0],
-         [0  ,                  0    ,                0     ,           1    ]])
-
-T7_F =  sym.Matrix([[1,                 0,                    0,                0    ],
-         [0*1,  1* 1,  -0, -0*0.333],
-         [0*0,  1* 0,   1,  1*0.333],
-         [0  ,                  0    ,                0     ,           1    ]])
+    move_group.clear_pose_targets()
 
 
-T0_3 = T0_1*T1_2*T2_3
-T0_F = T0_3*T3_4*T4_5*T5_6*T6_7*T7_F
+    
 
-print (T0_3)
 
-print('')
-
-print (simplify_matrix(T0_F))
 
 
 
